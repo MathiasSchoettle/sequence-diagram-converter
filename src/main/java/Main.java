ac83493eab2c29b3f6f543c205ee9835cb5ebdbd
Mathias Schoettle <mathias.schoettle@st.oth-regensburg.de>
2022-11-16 14:59:55 +0100

    added more examples

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,11 @@ import sequenceDiagram.SequenceDiagramConverter;
 import sequenceDiagram.SequenceDiagramImport;
 
 public class Main {
+
+    public static final String FILE_NAME = "bsp_3";
     public static void main(String[] args) {
-        var imported = SequenceDiagramImport.fromFile("lifelines.json");
+        var imported = SequenceDiagramImport.fromFile(FILE_NAME + ".json");
         var automaton = new SequenceDiagramConverter(imported).toAutomaton();
-        automaton.exportToGraphviz("aufgabe_vorlesung");
+        automaton.exportToGraphviz(FILE_NAME);
     }
 }
